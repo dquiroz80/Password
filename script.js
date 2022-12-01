76a97e07f21b27a00d48127cabdc693c12c48773
Quiroz <jodia.quiroz@cms.k12.nc.us>
2022-12-01 10:32:31 -0500

    fixing issues

--- script.js
+++ script.js
@@ -4,49 +4,58 @@ function generatePassword() {
   
   // YOUR CODE GOES HERE
   var special = "!#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
-  var letters = "abcdefghijklmnopqrstuvwxyz";
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   var numbers = "0987654321";
   var password = "";
-  var upLetters = letters.toUpperCase();
+  var characters = "";
   var isUppercase =  confirm("Would you like to use uppercase?");
   var isLowercase = confirm("Would you like to use lowercase?");
   var isSpecial = confirm("Would you like to use special characters?");
   var isNumber = confirm("Would you like to use numbers?");
-  var isLength = prompt("Choose password length from 8 to 128");
+  var isLength = parseInt(prompt("Choose password length from 8 to 128"));
+
+
     if (isLength < 8 || isLength > 128) { 
         alert("Please choose a number between 8 and 128.");
+        return "";
     }
   
     if (isUppercase) {
-        password += letters
+        characters += upper;
+        ensure += upper[random];
+
     }else{
   
     }
   
     if (isLowercase) {
-        password += letters
+        characters += lower
     }else{
   
     }
   
     if (isSpecial) {
-        password += special
+        characters += special
     }else{
   
     }
   
     if (isNumber) {
-        password += numbers
+        characters += numbers
     }else{
   
     }
-    var hereItIs = "";
-  
-    for (var i =0; i , isLength; i++); {
-      hereItIs += password.charAt(Math.floor(Math.random() * password.length)); 
-    }
 
-    return password;
+    if (isUppercase || isLowercase || isSpecial || isNumber) {
+      var random = Math.floor(Math.random() * characters.length);
+      password += character{random}
+    }else{
+      alert("Please choose one option");
+    }
+    
+
+    return password.slice(ensure.length)+=(ensure);
   //Grab Random letters
    
     
